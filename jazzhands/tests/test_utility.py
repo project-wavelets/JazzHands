38933528165ac04782d7c5c275c5ebde5e08450f
Athul R T <athul@disroot.org>
2020-09-09 16:50:54 +0530

    modified test file for utility.py by adding comments

--- jazzhands/tests/test_utility.py
+++ jazzhands/tests/test_utility.py
@@ -3,19 +3,22 @@ import numpy as np
 import pytest
 
 def test_phi_1():
-	phi1 = utility.phi_1([1,2,3], 3, 10)
-	expected = np.array([1.,1.,1.])
-	assert np.allclose(phi1,expected)
-	
+    """Test to make sure the output is an array of ones"""
+    phi1 = utility.phi_1([1,2,3], 3, 10)
+    expected = np.array([1.,1.,1.])
+    assert np.allclose(phi1,expected)
+    
 def test_phi_2():
-	time = np.array([1,2,3])
-	phi2 = utility.phi_2(time, 3, 10)
-	expected = np.array([ 1., -0.99, 0.96])
-	assert pytest.approx(phi2,expected)
+    """Test to make sure the function output gives an approximate expected result"""
+    time = np.array([1,2,3])
+    phi2 = utility.phi_2(time, 3, 10)
+    expected = np.array([ 1., -0.99, 0.96])
+    assert pytest.approx(phi2,expected)
 
 
 def test_phi_3():
-	time = np.array([1,2,3])
-	phi3 = utility.phi_3(time, 3, 10)
-	expected = np.array([ 0., 0.14, -0.28])
-	assert pytest.approx(phi3,expected)
+    """Test to make sure the function output gives an approximate expected result"""
+    time = np.array([1,2,3])
+    phi3 = utility.phi_3(time, 3, 10)
+    expected = np.array([ 0., 0.14, -0.28])
+    assert pytest.approx(phi3,expected)
