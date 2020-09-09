7e12cecacf8c47c818d8e11cfa84c0c1838a9ebf
Amogh Desai <amoghd9@gmail.com>
2020-09-10 04:07:32 +0530

    Deleted utils folder and changed tests

--- jazzhands/__init__.py
+++ jazzhands/__init__.py
@@ -3,12 +3,12 @@
 # Packages may add whatever they like to this file, but
 # should keep this content at the top.
 # ----------------------------------------------------------------------------
-from ._astropy_init import *   # noqa
+from ._astropy_init import *  # noqa
 # ----------------------------------------------------------------------------
 
 __all__ = []
-from .wavelets import *   # noqa
-from .utility import *
+from jazzhands.wavelets import *  # noqa
+from jazzhands.utils import *
 # Then you can be explicit to control what ends up in the namespace,
 # or you can keep everything from the subpackage with the following instead
 __all__ += wavelets.__all__
