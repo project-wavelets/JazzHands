dc7acd2fb3ab66750de47ae986c4e34b213e8cbf
Trevor Dorn-Wallenstein <trevorzaylen@gmail.com>
2020-09-15 10:55:11 -0700

    adding in version number, standin for now

--- jazzhands/__init__.py
+++ jazzhands/__init__.py
@@ -10,3 +10,6 @@ from jazzhands.utils import *
 # Then you can be explicit to control what ends up in the namespace,
 # or you can keep everything from the subpackage with the following instead
 __all__ += wavelets.__all__
+
+#Version number
+__version__ = '0.0.1'
