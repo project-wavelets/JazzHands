66a43ccc92ba547fc6be2db67e059e84a71dfacf
Aditya Sengupta <aditya.sengupta@berkeley.edu>
2020-09-28 13:01:06 -0700

    merge main and vectorize

--- jazzhands/tests/test_wavelets.py
+++ jazzhands/tests/test_wavelets.py
@@ -1,6 +1,8 @@
 import os
 import shutil
 import unittest
+import numpy as np
+from tqdm import tqdm
 
 from jazzhands import wavelets
 
@@ -15,27 +17,25 @@ class WaveletsTest(unittest.TestCase):
         if os.path.exists(self.outdir):
             shutil.rmtree(self.outdir)
 
-    def test_correct_constructor(self):
-        wavelets.WaveletTransformer(func_list=[0, 0], f1=[0, 0], data=[0, 0],
-                                    time=[0, 0], omegas=[1, 2], taus=[0, 0.5], c=0.0125)
+    def make_test_time_signal(self):
+        # Can switch this out for something else later
+        num_steps = 100
+        t = np.linspace(-1, 1, num_steps)
+        freq = 1
+        phase = np.pi / 4
+        x = np.cos(2 * np.pi * freq * t + phase)
+        return t, x
 
-    def test_wavelet_transform_no_gap(self):
-        import numpy as np
-        from tqdm import tqdm
-
-        def make_test_time_signal():
-            # Can switch this out for something else later
-            num_steps = 100
-            t = np.linspace(-1, 1, num_steps)
-            freq = 1
-            phase = np.pi / 4
-            x = np.cos(2 * np.pi * freq * t + phase)
-            return t, x
-
-        t, x = make_test_time_signal()
+    def get_params_and_data(self):
+        t, x = self.make_test_time_signal()
         omegas = [1.1, 1.4, 1.8, 2.0]
         taus = [0.0, 0.5]
         c = 1 / (8 * np.pi ** 2)
+        return t, x, omegas, taus, c
+
+    def make_transformer(self):
+        # not its own test, but is executed by later tests
+        t, x, omegas, taus, c = self.get_params_and_data()
 
         transformer = wavelets.WaveletTransformer(
             time = t, 
@@ -44,6 +44,15 @@ class WaveletsTest(unittest.TestCase):
             taus = taus, 
             c = c
         )
+        
+        return transformer
+
+    def test_make_transformer(self):
+        self.make_transformer()
+
+    def test_wavelet_transform_no_gap(self):
+        self.transformer = self.make_transformer()
+        t, x, omegas, taus, c = self.get_params_and_data()
 
         def slow_wwz_and_checks(t, x, omegas, taus, c):
             # naive/not well encapsulated/not parallelized implementation, just to check correctness
@@ -58,34 +67,34 @@ class WaveletsTest(unittest.TestCase):
                     weights = np.exp(-c * zs ** 2)
                     weights /= np.sum(weights)
                     npoints = 1 / np.inner(weights, weights)
-                    assert np.allclose(weights, transformer._weight_alpha(t, omega, tau, c))
-                    assert np.isclose(transformer._n_points(weights), npoints)
+                    assert np.allclose(weights, self.transformer._weight_alpha(t, omega, tau, c))
+                    assert np.isclose(self.transformer._n_points(weights), npoints)
 
                     S = np.array([[np.sum(basis_evals[k] * basis_evals[l] * weights) for k in range(3)] for l in range(3)])
-                    assert np.allclose(transformer._S_matrix(basis_evals, weights), S)
+                    assert np.allclose(self.transformer._S_matrix(basis_evals, weights), S)
 
                     sols = np.sum(basis_evals * weights * x, axis=1)
                     yc = np.linalg.inv(S).dot(sols)
                     y = yc.dot(basis_evals)
-                    y_package, yc_package = transformer._y_fit(basis_evals, weights, x)
+                    y_package, yc_package = self.transformer._y_fit(basis_evals, weights, x)
                     assert np.allclose(yc, yc_package)
                     assert np.allclose(y, y_package)
                     
                     vx = np.sum(weights * x * x) - np.dot(weights, x) ** 2
                     vy = np.sum(weights * y * y) - np.dot(weights, y) ** 2
                     
-                    assert np.isclose(vx, transformer._weight_var_x(np.ones(len(x)), weights, x))
-                    assert np.isclose(vy, transformer._weight_var_y(basis_evals, np.ones(len(x)), weights, x)[0])
+                    assert np.isclose(vx, self.transformer._weight_var_x(np.ones(len(x)), weights, x))
+                    assert np.isclose(vy, self.transformer._weight_var_y(basis_evals, np.ones(len(x)), weights, x)[0])
 
                     transforms[i][j] = (npoints - 3) * vy / (2 * (vx - vy))
-                    assert np.isclose(transforms[i][j], transformer._wavelet_transform(True, tau, omega)[0])
+                    assert np.isclose(transforms[i][j], self.transformer._wavelet_transform(True, tau, omega)[0])
             return transforms
 
-        wwz, wwa = transformer.compute_wavelet()
-        wwz_check = slow_wwz_and_checks(t, x, omegas, taus, c)
-        
-        print(wwz, '\n', wwz_check)
-        assert np.allclose(wwz, wwz_check)
+        for v in [False, True]:
+            wwz, wwa = self.transformer.compute_wavelet(vectorized=v)
+            wwz_check = slow_wwz_and_checks(t, x, omegas, taus, c)
+            
+            assert np.allclose(wwz, wwz_check)
 
     # def test_omegas_taus_from_min_max_nu(self):
     #     wav = wavelets.WaveletTransformer(func_list=[0, 0], f1=[0, 0], data=[0, 0], time=[0, 0], omegas=[0, 0], taus=[0, 0], c=0.0125)
--- jazzhands/wavelets.py
+++ jazzhands/wavelets.py
@@ -290,7 +290,7 @@ class WaveletTransformer:
         S = np.zeros((l, l))
         for i in range(l):
             for j in range(i + 1):
-                S[i][j] = self._inner_product(func_vals[i], func_vals[j], weights)
+                S[i][j] = (func_vals[i] * func_vals[j]).dot(weights)
 
         S = S + S.T - np.diag(S.diagonal())
         return S
@@ -452,7 +452,7 @@ class WaveletTransformer:
         return ((num_pts - 3.0) * y_var) / (2.0 * (x_var - y_var)), np.sqrt(
             np.power(y_coeff_rows[1], 2.0) + np.power(y_coeff_rows[2], 2.0))
 
-    def compute_wavelet(self, exclude=True, parallel=False, n_processes=False, vectorized=True):
+    def compute_wavelet(self, exclude=True, parallel=False, n_processes=False, vectorized=False):
         """
         Calculate the Weighted Wavelet Transform of the object. Note that this
         can be incredibly slow for a large enough data array and a dense
@@ -510,27 +510,36 @@ class WaveletTransformer:
 
         else: 
             if vectorized:
+                from scipy.special import softmax
                 omegas, taus, time = np.meshgrid(self._omegas, self._taus, self._time, indexing='ij')
                 zvals_grid = omegas * (time - taus)
-                weights_grid = np.exp(-self._c * np.power(zvals_grid, 2.))
-                weights_grid /= weights_grid.sum(axis=-1)[:,:,np.newaxis]
+                weights_grid = softmax(-self._c * np.power(zvals_grid, 2.), axis=-1)
                 npoints = 1 / np.sum(weights_grid ** 2, axis=-1)
-                
                 func_vals = np.array([func(zvals_grid) for func in [np.ones_like, np.sin, np.cos]])
+                x_var = weights_grid.dot(self._data ** 2) - (weights_grid.dot(self._data)) ** 2
                 
-                f1_vals = func_vals[0]
-                get_wvar_x = lambda weights: self._weight_var_x(f1_vals[0,0], weights, self._data)
-                x_var = np.apply_along_axis(get_wvar_x, -1, weights_grid)
-
                 S_matrices = np.array([[self._S_matrix(func_vals[:,i,j,:], weights_grid[i,j]) for j in range(len(self._taus))] for i in range(len(self._omegas))])
                 phis = np.array([[self._inner_product_vector(func_vals[:,i,j,:], weights_grid[i,j], self._data) for j in range(len(self._taus))] for i in range(len(self._omegas))])
+
+                for i, omega in enumerate(tqdm(self._omegas)):
+                    for j, tau in enumerate(self._taus):
+                        local_weights = self._weight_alpha(self._time, omega, tau, self._c)
+                        local_z = omega * (self._time - tau)
+                        local_func_vals = np.array([np.ones_like(local_z), np.sin(local_z), np.cos(local_z)])
+                        local_S = self._S_matrix(local_func_vals, local_weights)
+                        try:
+                            np.linalg.solve(local_S, phis[i,j])
+                        except np.linalg.LinAlgError:
+                            print(i, j, omega, tau)
+                            self._calc_coeffs(local_func_vals, local_weights, self._data)
+                        
+                
                 y_coeffs = np.array([[np.linalg.solve(S_matrices[i,j], phis[i,j]).T[0] for j in range(len(self._taus))] for i in range(len(self._omegas))])
                 y_fit = np.einsum('jki,ijkl->jkl', y_coeffs, func_vals)
                 
                 get_wvar_y = lambda weights, fit_vals: self._inner_product(fit_vals, fit_vals, weights) - np.power(self._inner_product(fit_vals, weights), 2.0)
                 y_var = np.array([[get_wvar_y(weights_grid[i,j], y_fit[i,j]) for j in range(len(self._taus))] for i in range(len(self._omegas))])
 
-                # dinosaur
                 for i, omega in enumerate(self._omegas):
                     for j, tau in enumerate(self._taus):
                         assert np.isclose(self._n_points(weights_grid[i][j]), npoints[i][j])
@@ -539,8 +548,7 @@ class WaveletTransformer:
                         local_func_vals = np.array([np.ones_like(z), np.sin(z), np.cos(z)])
                         assert np.allclose(func_vals[:,i,j,:], local_func_vals)
                         assert np.allclose(weight_vector, weights_grid[i][j])
-                        assert np.allclose(get_wvar_x(weights_grid[i][j]), self._weight_var_x(np.ones_like(self._time), weight_vector, self._data))
-                        assert np.allclose(get_wvar_x(weights_grid[i][j]), x_var[i][j])
+                        assert np.allclose(self._weight_var_x(np.ones_like(self._time), weight_vector, self._data), x_var[i][j])
                         assert np.allclose(S_matrices[i,j], self._S_matrix(local_func_vals, weight_vector))
                         local_coeffs = self._calc_coeffs(local_func_vals, weight_vector, self._data)
                         assert np.allclose(y_coeffs[i,j], local_coeffs)
@@ -549,7 +557,7 @@ class WaveletTransformer:
                         local_yvar_2 = self._inner_product(local_fit, local_fit, weight_vector) - np.power(self._inner_product(local_fit, weight_vector), 2.0)
                         assert np.allclose(local_fit, y_fit[i,j])
                         assert np.isclose(local_yvar, local_yvar_2)
-                        assert np.isclose(local_yvar, y_var[i,j])                   
+                        assert np.isclose(local_yvar, y_var[i,j])  
                 
                 wwz = ((npoints - 3.0) * y_var) / (2.0 * (x_var - y_var))
                 wwa = np.sqrt(np.power(y_coeffs[:,:,1], 2.0) + np.power(y_coeffs[:,:,2], 2.0))
