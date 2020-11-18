83dd9b08414236dd8099fc20debc26f4b9212c3d
Trevor Dorn-Wallenstein <trevorzaylen@gmail.com>
2020-11-17 17:21:36 -0800

    updated version number

--- jazzhands/__init__.py
+++ jazzhands/__init__.py
@@ -12,7 +12,7 @@ from jazzhands.utils import *
 try:
     from jazzhands.version import version
 except ModuleNotFoundError:
-    version = '0.0.4'
+    version = '0.0.3'
 # Then you can be explicit to control what ends up in the namespace,
 # or you can keep everything from the subpackage with the following instead
 __all__ += wavelets.__all__
